7378c6b68d34ec4b67683d7c0273bbe86bd07158
pahagon <paulo.ahagon@gmail.com>
2022-08-10 09:29:14 -0300

    feat: Add function that checks if a recipe is valid

--- umamify/meal_planner/lib/meal_planner/recipe.ex
+++ umamify/meal_planner/lib/meal_planner/recipe.ex
@@ -11,15 +11,19 @@ defmodule MealPlanner.Recipe.Item do
   This function check if is an item is valid
 
   ## Examples
-      iex> r_item = %MealPlanner.Recipe.Item{name: "meat", qty: 1.0, unit: :kg} 
+      iex> r_item = %MealPlanner.Recipe.Item{name: "meat", qty: 1.0, unit: :kg}
       iex> MealPlanner.Recipe.Item.valid?(r_item)
       {:ok, r_item}
 
-      iex> r_item = %MealPlanner.Recipe.Item{} 
+      iex> r_item = %MealPlanner.Recipe.Item{}
       iex> MealPlanner.Recipe.Item.valid?(r_item)
       {:error, "unit is nil, qty is nil, name is nil", r_item}
 
-      iex> r_item = %MealPlanner.Recipe.Item{name: "", qty: 1.0, unit: :kg} 
+      iex> r_item = %MealPlanner.Recipe.Item{qty: 1.0, unit: :kg}
+      iex> MealPlanner.Recipe.Item.valid?(r_item)
+      {:error, "name is nil", r_item}
+
+      iex> r_item = %MealPlanner.Recipe.Item{name: "", qty: 1.0, unit: :kg}
       iex> MealPlanner.Recipe.Item.valid?(r_item)
       {:error, "name is empty", r_item}
 
@@ -71,15 +75,33 @@ defmodule MealPlanner.Recipe.Item do
   This function check if is an item is valid
 
   ## Examples
-      iex> item_1 = %MealPlanner.Recipe.Item{name: "meat", qty: 1.0, unit: :kg} 
-      iex> item_2 = %MealPlanner.Recipe.Item{name: "chicken", qty: 2.0, unit: :kg} 
-      iex> MealPlanner.Recipe.Item.any?([item_1, item_2])
+      iex> item_1 = %MealPlanner.Recipe.Item{name: "meat", qty: 1.0, unit: :kg}
+      iex> item_2 = %MealPlanner.Recipe.Item{name: "chicken", qty: 2.0, unit: :kg}
+      iex> MealPlanner.Recipe.Item.any_invalid?([item_1, item_2])
       {:ok, [item_1, item_2]}
+
+      iex> item_3 = %MealPlanner.Recipe.Item{}
+      iex> MealPlanner.Recipe.Item.any_invalid?([item_3])
+      {:error, [item_3]}
   """
-  @spec valid?(list(__MODULE__.t())) ::
+  @spec any_invalid?(list(__MODULE__.t())) ::
           {:ok, list(__MODULE__.t())} | {:error, list(__MODULE__.t())}
-  def any?(items) do
-    if Enum.any?(items, fn item -> valid?(item) end) do
+  def any_invalid?(items) do
+    item =
+      Enum.find(
+        items,
+        fn item ->
+          case valid?(item) do
+            {:error, _msg, _item} ->
+              true
+
+            _ ->
+              false
+          end
+        end
+      )
+
+    if is_nil(item) do
       {:ok, items}
     else
       {:error, items}
@@ -97,4 +119,78 @@ defmodule MealPlanner.Recipe do
   Type that represents Recipe
   """
   @type t :: %__MODULE__{title: String.t(), instructions: String.t(), ingredients: list(Item.t())}
+
+  @doc ~S"""
+  This function check if is a recipe is valid
+
+  ## Examples
+      iex> item_1 = %MealPlanner.Recipe.Item{name: "pasta", qty: 0.1, unit: :kg}
+      iex> item_2 = %MealPlanner.Recipe.Item{name: "chicken", qty: 0.3, unit: :kg}
+      iex> itens = [item_1, item_2]
+      iex> recipe = %MealPlanner.Recipe{title: "Chicken Pasta", instructions: "Cookie", ingredients: itens}
+      iex> MealPlanner.Recipe.valid?(recipe)
+      {:ok, recipe}
+
+      iex> item_1 = %MealPlanner.Recipe.Item{}
+      iex> item_2 = %MealPlanner.Recipe.Item{name: "chicken", qty: 0.3, unit: :kg}
+      iex> itens = [item_1, item_2]
+      iex> recipe = %MealPlanner.Recipe{title: "Chicken Pasta", instructions: "Cookie", ingredients: itens}
+      iex> MealPlanner.Recipe.valid?(recipe)
+      {:error, "There are/is itens with error", recipe}
+
+      iex> recipe = %MealPlanner.Recipe{}
+      iex> MealPlanner.Recipe.valid?(recipe)
+      {:error, "title is nil, instructions is nil, ingredients is nil", recipe}
+
+      iex> MealPlanner.Recipe.valid?(nil)
+      {:error, "recipe is nil", nil}
+  """
+  @spec valid?(__MODULE__.t()) :: {:ok, __MODULE__.t()} | {:error, String.t(), __MODULE__.t()}
+  def valid?(recipe) do
+    cond do
+      recipe == nil ->
+        {:error, "recipe is nil", nil}
+
+      recipe != nil ->
+        nil_attrs = filter_nil_attrs(recipe)
+
+        if Enum.empty?(nil_attrs) do
+          case Item.any_invalid?(recipe.ingredients) do
+            {:error, _items} ->
+              {:error, "There are/is itens with error", recipe}
+
+            _ ->
+              {:ok, recipe}
+          end
+        else
+          {:error, Enum.join(nil_attrs, ", "), recipe}
+        end
+
+      # best pratice
+      true ->
+        {:error, "Unknown", recipe}
+    end
+  end
+
+  defp filter_nil_attrs(recipe) do
+    recipe
+    |> Map.keys()
+    |> Enum.reduce(
+      [],
+      fn attr, acc ->
+        value = Map.get(recipe, attr)
+
+        cond do
+          is_nil(value) ->
+            ["#{attr} is nil" | acc]
+
+          is_binary(value) && String.trim(value) == "" ->
+            ["#{attr} is empty" | acc]
+
+          true ->
+            acc
+        end
+      end
+    )
+  end
 end
